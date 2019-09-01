a623a4a938432749570e8024e9cb2a4d358c961e
anushaprajapati <prz.anusha@gmail.com>
2019-08-31 22:23:58 -0400

    first commit

---
+++ src/main/java/com/jokeapp/UdemyJokeApp/UdemyJokeAppApplication.java
@@ -0,0 +1,13 @@
+package com.jokeapp.UdemyJokeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyJokeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyJokeAppApplication.class, args);
+	}
+
+}
